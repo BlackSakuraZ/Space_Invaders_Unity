6708d0987c40cda2056d73f2631bf93311290549
BlackSakuraZ <78552049+BlackSakuraZ@users.noreply.github.com>
2022-10-19 23:04:53 +0300

    Delete Scripts directory

--- Scripts/Restart.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-using System.Collections;
-     
-public class Restart : MonoBehaviour {
-    public void RestartGame() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-}
